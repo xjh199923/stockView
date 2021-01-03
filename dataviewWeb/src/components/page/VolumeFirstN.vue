fa9ebd8436aff736685f8b5931dc29226c9459b9
xjh199923 <981715157@qq.com>
2021-01-03 12:31:05 +0800

    更新了成交量前N排行界面

---
+++ dataviewWeb/src/components/page/VolumeFirstN.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="stockVolume">
+        <el-container>
+            <el-container style="width:100%;">
+                    <el-row :gutter="100">
+                        <el-col>
+                            <div id="echartContainer" ref="echartContainer" style="width:1000px; height:580px"></div>
+                        </el-col>
+                    </el-row> 
+            </el-container>
+        </el-container>
+    </div>
+    
+</template>
+
+<script>
+import axios from 'axios'
+var echarts = require("echarts");
+export default {
+  data() {
+    return {
+        data: "",
+    };
+  },
+
+  methods: {
+      getMessage() {
+        // 使用 axios 向 flask 发送请求
+        const url = "http://127.0.0.1:8888/volumeFirstN/5";
+        console.log(url);
+        axios.get(url).then((res) => {
+                
+                var tmpNamelist = res.data.firstNnameList;
+                var tmpDatelist = res.data.firstNdateList;
+                var tmpfirstNdata = res.data.firstNdataList;
+
+                var seriseData = []
+                for(let i = 0; i < tmpNamelist.length; i++){
+                    let tmpdic = { 
+                        name: tmpNamelist[i],
+                        type: 'line',
+                        data: tmpfirstNdata[i]
+                    };
+                    seriseData.push(tmpdic);
+                }
+
+                var colors = ['#5793f3', '#d14a61', '#675bba'];
+                var option = {
+                title: {
+                    text: '成交量排名前N支股票',
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: tmpNamelist
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        dataView: {show: true, readOnly: false},
+                        restore: {show: true},
+                        saveAsImage: {show: true}
+                    }
+                },
+                dataZoom: [
+                {
+                    type: 'inside',
+                    start: 30,
+                    end: 100
+                },
+                {
+                    show: true,
+                    type: 'slider',
+                    top: '100%',
+                    start: 30,
+                    end: 100
+                }
+                ],
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: tmpDatelist
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: seriseData
+            };       
+            var charts = echarts.init(this.$refs.echartContainer);
+            if (option && typeof option === "object") {
+                charts.setOption(option, true);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+  },
+  //在模板渲染成html前调用生命周期函数
+  created() {
+    this.getMessage();
+  },
+}
+</script>
+<style>
+    .stockVolume{
+        position: absolute;
+        left: 50px;
+    }
+</style>
+
+      
